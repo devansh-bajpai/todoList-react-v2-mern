97d1c211ddbd115c3197c03c73301b00eff8bea2
Devansh Bajpai <devanshb.code@gmail.com>
2025-03-18 19:52:05 +0530

    added basic structure

---
+++ client/my-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import Login from './components/Login';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+
+      <Routes>
+      <Route path='/login' element={<Login/>}/>
+      </Routes>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/my-app/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='bg-white h-100 w-150 rounded-md'>
+
+    </div>
+  )
+}
